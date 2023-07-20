eadd2ec5a9b5d91aa4701d0fe934dac1cb46a1aa
Abhit03 <abhit95@gmail.com>
2023-07-20 16:39:50 +0200

    Chnage the styling - 1. Make the +/- symbols bold text 2. Make the name text larger

--- src/pages/Tree/Tree.tsx
+++ src/pages/Tree/Tree.tsx
@@ -30,19 +30,19 @@ const Tree: React.FC<TreeProps> = ({ item, onItemNameClick }) => {
         <div
           onClick={handleSymbolClick}
           className={
-            'cursor-pointer inline-flex justify-center items-center w-5 h-5 border border-black mr-1 text-lg bg-black text-white rounded-full pb-1'
+            'cursor-pointer inline-flex justify-center items-center w-5 h-5 border border-black mr-1 text-xl bg-black text-white font-bold rounded-full pb-1'
           }
         >
           {isOpen ? '-' : '+'}
         </div>
         <span
-          className={`inline-block w-5 h-5 mr-1 rounded ${
+          className={`inline-block w-8 h-6 mr-1 rounded ${
             item.status.toLowerCase() === 'active'
               ? 'bg-green-500'
               : 'bg-red-500'
           }`}
         ></span>
-        <span onClick={handleNameClick} className="cursor-pointer">
+        <span onClick={handleNameClick} className="cursor-pointer text-lg">
           {item.name}
         </span>
       </div>
