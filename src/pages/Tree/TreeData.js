9d4967d1fe2822830ca3411163a70ea339035502
Abhit03 <abhit95@gmail.com>
2023-07-20 13:45:44 +0200

    Large randomised device tree for showcase

--- src/pages/Tree/TreeData.js
+++ src/pages/Tree/TreeData.js
@@ -1,24 +1,20 @@
+function randomStatus() {
+  return Math.random() > 0.5 ? 'active' : 'inactive';
+}
+
 export const treeData = {
   name: 'Root',
-  status: 'active',
-  children: [
-    {
-      name: 'Child 1',
-      status: 'inactive',
-      children: [
-        {
-          name: 'Grandchild 1',
-          status: 'active'
-        },
-        {
-          name: 'Grandchild 2',
-          status: 'inactive'
-        }
-      ]
-    },
-    {
-      name: 'Child 2',
-      status: 'active'
-    }
-  ]
+  status: randomStatus(),
+  children: Array.from({ length: 10 }, (_, i) => ({
+    name: `Child ${i + 1}`,
+    status: randomStatus(),
+    children: Array.from({ length: 10 }, (_, j) => ({
+      name: `Grandchild ${i * 10 + j + 1}`,
+      status: randomStatus(),
+      children: Array.from({ length: 100 }, (_, k) => ({
+        name: `Great Grandchild ${i * 1000 + j * 100 + k + 1}`,
+        status: randomStatus()
+      }))
+    }))
+  }))
 };
