90ac2986d6408ac3ba9b769f5c8d21c61323f9c0
Abhit03 <abhit95@gmail.com>
2023-08-10 02:07:13 +0200

    Align output row

--- src/app/common/rows/OutputRow.tsx
+++ src/app/common/rows/OutputRow.tsx
@@ -8,7 +8,7 @@ const opts: SelectOption[] = [
 
 const OutputRow = () => (
   <div className="flex items-center p-2 ml-[24px] mr-[8px] bg-amber-300 rounded-xl border-2 border-black">
-    <h1 className="pl-8">Output state is</h1>
+    <h1 className="pl-4">Output state is</h1>
     <SelectButton text={'Output state'} options={opts} />
   </div>
 );
