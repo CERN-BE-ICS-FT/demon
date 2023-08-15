2917e11e8adfb3411d37151196d625a2e329ea7b
Abhit03 <abhit95@gmail.com>
2023-08-15 21:46:23 +0200

    Make tree resizable with Panels

--- src/app/global/layouts/RootLayout.tsx
+++ src/app/global/layouts/RootLayout.tsx
@@ -2,13 +2,49 @@ import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import Footer from '../footer/Footer';
 import Tree from '../../../pages/Tree/Tree';
 import { treeData } from '../../../pages/Tree/TreeData';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import TreeNavBar from '../../../pages/Tree/TreeNavBar';
 import TreeIconsRow from '../../common/rows/TreeIconsRow';
 import Navbar from '../navbar/Navbar';
+import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels';
+
+const DEFAULT_LEFT_PANEL_SIZE = 21;
+
+import React from 'react';
+
+interface ResizeHandleProps {
+  collapsed: boolean;
+  expandPanel: () => void;
+}
+
+const ResizeHandle: React.FC<ResizeHandleProps> = ({
+  collapsed,
+  expandPanel
+}) => {
+  if (collapsed) {
+    return (
+      <div
+        onClick={expandPanel}
+        className="relative cursor-e-resize flex h-screen w-5 ml-5 flex-row items-center justify-center gap-px rounded-sm hover:bg-zinc-200 transition duration-200 text-transparent hover:text-black font-bold"
+      >
+        <span>&gt;</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="relative cursor-col-resize flex h-screen w-1 flex-row items-center justify-center gap-px rounded-sm bg-zinc-200 hover:bg-zinc-800 transition duration-200">
+        <div className="h-4 w-0.5 bg-zinc-200" />
+      </div>
+    );
+  }
+};
 
 export default function RootLayout() {
+  const [leftSize, setLeftSize] = useState(DEFAULT_LEFT_PANEL_SIZE);
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const [selectedItem, setSelectedItem] = useState('');
+
+  const panelRef = useRef<any>(null); // Reference to Panel
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -44,13 +80,29 @@ export default function RootLayout() {
     }
   };
 
+  const handleExpandPanel = () => {
+    if (panelRef.current) {
+      panelRef.current.resize(DEFAULT_LEFT_PANEL_SIZE);
+      setIsCollapsed(false);
+    }
+  };
+
   return (
     <div className="root-layout min-h-screen min-w-screen bg-white">
       <Navbar></Navbar>
-      <div className="container flex-grow flex w-screen min-h-[calc(88vh)]">
-        {!isInSettings && (
-          // <aside className="flex-grow bg-zinc-200 min-w-[340px] border-r-2 border-zinc-800 p-2 flex flex-col items-start w-fit max-w-[300px]">
-          <aside className="flex-grow bg-zinc-200 min-w-[340px]  p-2 flex flex-col items-start w-fit max-w-[300px]">
+      <div className="container flex-grow flex w-screen h-screen">
+        <PanelGroup direction="horizontal" className="relative">
+          <Panel
+            ref={panelRef}
+            defaultSize={DEFAULT_LEFT_PANEL_SIZE}
+            order={1}
+            minSize={10}
+            onResize={setLeftSize}
+            collapsible={true}
+            collapsedSize={0}
+            onCollapse={setIsCollapsed}
+            className="bg-zinc-200 flex-grow"
+          >
             <TreeNavBar />
             {pathParts[1] !== 'monitor' ? (
               <TreeIconsRow
@@ -77,11 +129,24 @@ export default function RootLayout() {
                 ></Tree>
               )}
             </h1>
-          </aside>
-        )}
-        <main className="p-4 flex-grow bg-white">
-          <Outlet />
-        </main>
+          </Panel>
+          <PanelResizeHandle
+            className="absolute top-1/2 -translate-x-1/2 -translate-y-1/2"
+            style={{
+              left: `${leftSize}%`
+            }}
+          >
+            <ResizeHandle
+              collapsed={isCollapsed}
+              expandPanel={handleExpandPanel}
+            />
+          </PanelResizeHandle>
+          <Panel order={2}>
+            <main className="p-4 flex-grow bg-white">
+              <Outlet />
+            </main>
+          </Panel>
+        </PanelGroup>
       </div>
       <Footer />
     </div>
